3e112cb00c36c8c8095ae73375f481715127db49
shahd <shahd1701605@miuegypt.edu.eg>
2021-06-01 16:08:03 +0200

    .

--- lib/TextAudio.dart
+++ lib/TextAudio.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:ui_gp/predict.dart';
 
 class TextAudio extends StatefulWidget {
   static const routeName = '/extractName';
@@ -24,7 +25,9 @@ class _TextAudioState extends State<TextAudio> {
     super.initState();
   }
   @override
+  
   Widget build(BuildContext context) {
+     final args = ModalRoute.of(context).settings.arguments as Predict;
     return Scaffold(
       
      appBar: AppBar(
@@ -32,7 +35,7 @@ class _TextAudioState extends State<TextAudio> {
             color: Colors.black, //change your color here
           ),
           title: Text(
-            "Detection",
+            "Detection"+args.name,
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Colors.black,
