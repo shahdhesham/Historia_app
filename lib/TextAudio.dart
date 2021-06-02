d7fbff78fa9ef49f5f02552b35407ba471e46cee
shahd <shahd1701605@miuegypt.edu.eg>
2021-06-02 14:59:45 +0200

    TextAudio file

--- lib/TextAudio.dart
+++ lib/TextAudio.dart
@@ -1,77 +1,168 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:ui_gp/predict.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
 
 class TextAudio extends StatefulWidget {
-  static const routeName = '/extractName';
   @override
   _TextAudioState createState() => _TextAudioState();
 }
 
-class _TextAudioState extends State<TextAudio> {
-    String data = '';
+enum TtsState { playing, stopped, continued }
 
-   fetchFileData() async {
-    String responseText;
-    print("tany 7eta");
-    responseText = await rootBundle.loadString('assets/Hanging Church.txt');
-    setState(() {
-      data = responseText;
+class _TextAudioState extends State<TextAudio> {
+   FlutterTts flutterTts;
+  String language;
+  String engine;
+  double volume = 1.0;
+  double pitch = 0;
+  double rate = 1.0;
+  bool isCurrentLanguageInstalled = false;
+
+
+  String _newVoiceText=" Here  Amr ibn el ass mosque ";
+
+  TtsState ttsState = TtsState.stopped;
+
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+  get isContinued => ttsState == TtsState.continued;
+
+  @override
+  initState() {
+    // _onChange(_newVoiceText);
+    super.initState();
+    initTts();
+  }
+
+  initTts() {
+    flutterTts = FlutterTts();
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
     });
   }
-   @override
-  void initState() {
-    fetchFileData();
-    super.initState();
+
+  Future _speak() async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(1);
+
+    if (_newVoiceText != null) {
+      if (_newVoiceText.isNotEmpty) {
+        await flutterTts.awaitSpeakCompletion(true);
+        await flutterTts.speak(_newVoiceText);
+      }
+    }
   }
+
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
   @override
-  
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  // void _onChange(String text) {
+  //   setState(() {
+  //     _newVoiceText = text;
+  //   });
+  // }
+  @override
   Widget build(BuildContext context) {
-     final args = ModalRoute.of(context).settings.arguments as Predict;
-    return Scaffold(
-      
-     appBar: AppBar(
-          iconTheme: IconThemeData(
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+               iconTheme: IconThemeData(
             color: Colors.black, //change your color here
           ),
           title: Text(
-            "Detection"+args.name,
+            "Learn More",
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Colors.black,
           centerTitle: true,
         ),
-        body: ListView(
-          children: <Widget>[
-          
-           Row(
-              children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                  child: Container(
-                      child: Text(data)
-                  )
-                  ),
-            ]),
-            Row(
-              children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 20, 0),
-                  child: Container(
-                      child: ElevatedButton.icon(
-                    onPressed: () {
-                    },
-                    icon: Icon(Icons.volume_up_outlined),
-                    style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(),
-                      padding: EdgeInsets.all(24),
-                    ),
-                    label: Text(''),
-                  )
-                  )
-                  ),
-            ])
-          ],
-        ));
+            body: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(children: [
+                  _inputSection(),
+                  _btnSection(),
+               
+                ]))));
   }
+  Widget _inputSection() => Container(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),   
+      child:
+       Text(_newVoiceText
+        // onChanged: (String value) {
+        //   _onChange(value);
+        // },
+      )
+ 
+      );
+
+  Widget _btnSection() {
+      return Container(
+          padding: EdgeInsets.only(top: 50.0),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            _buildButtonColumn(Colors.green, Colors.greenAccent,
+                Icons.play_arrow, 'PLAY', _speak),
+            _buildButtonColumn(
+                Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
+          ])
+          );
+
+  }
+  Column _buildButtonColumn(Color color, Color splashColor, IconData icon,
+      String label, Function func) {
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+              icon: Icon(icon),
+              color: color,
+              splashColor: splashColor,
+              onPressed: () => func()),
+          Container(
+              margin: const EdgeInsets.only(top: 8.0),
+              child: Text(label,
+                  style: TextStyle(
+                      fontSize: 12.0,
+                      fontWeight: FontWeight.w400,
+                      color: color)))
+        ]);
+  }
+
 }
