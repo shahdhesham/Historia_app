0a1aeea66c1a9394a8b8e0350866cc71850693cf
shahd <shahd1701605@miuegypt.edu.eg>
2021-06-06 17:02:33 +0200

    builder

--- lib/TextAudio.dart
+++ lib/TextAudio.dart
@@ -2,7 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'my_shared_preferences.dart';
-
+import 'package:provider/provider.dart';
+import 'package:ui_gp/providers/monuments.dart';
 
 class TextAudio extends StatefulWidget {
   @override
@@ -10,16 +11,18 @@ class TextAudio extends StatefulWidget {
 }
 
 enum TtsState { playing, stopped, continued }
+
 class _TextAudioState extends State<TextAudio> {
-   @override
+  @override
   initState() {
     // _onChange(_newVoiceText);
     super.initState();
     initTts();
     // getStringValuesSF();
   }
+
 // String _email='';
-   FlutterTts flutterTts;
+  FlutterTts flutterTts;
   String language;
   String engine;
   double volume = 1.0;
@@ -28,7 +31,7 @@ class _TextAudioState extends State<TextAudio> {
   bool isCurrentLanguageInstalled = false;
 
   // String _newVoiceText="Here  Amr ibn el ass mosque ";
-  String _newVoiceText= "";
+  String _newVoiceText = "";
 
   TtsState ttsState = TtsState.stopped;
 
@@ -36,7 +39,6 @@ class _TextAudioState extends State<TextAudio> {
   get isStopped => ttsState == TtsState.stopped;
   get isContinued => ttsState == TtsState.continued;
 
- 
   initTts() {
     flutterTts = FlutterTts();
 
@@ -70,8 +72,9 @@ class _TextAudioState extends State<TextAudio> {
   }
 
   Future _speak() async {
-      MySharedPreferences.instance.getStringValue("name")
-      .then((value) => setState(() {
+    MySharedPreferences.instance
+        .getStringValue("name")
+        .then((value) => setState(() {
               _newVoiceText = value;
             }));
 
@@ -106,57 +109,69 @@ class _TextAudioState extends State<TextAudio> {
   // }
   @override
   Widget build(BuildContext context) {
-      MySharedPreferences.instance.getStringValue("name")
-      .then((value) => setState(() {
-              _newVoiceText = value;
-            }));
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-               iconTheme: IconThemeData(
-            color: Colors.black, //change your color here
-          ),
-          title: Text(
-            "Learn More",
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: Colors.black,
-          centerTitle: true,
-        ),
-            body: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(children: [
-                  _inputSection(),
-                  _btnSection(),
-               
-                ]))));
+              iconTheme: IconThemeData(
+                color: Colors.black, //change your color here
+              ),
+              title: Text(
+                "Learn More",
+                style: TextStyle(color: Colors.white),
+              ),
+              backgroundColor: Colors.black,
+              centerTitle: true,
+            ),
+            body: Builder(builder: (BuildContext context) {
+              MySharedPreferences.instance
+                  .getStringValue("name")
+                  .then((value) => setState(() {
+                        if (value == "HANGING CHURCH") {
+                          _newVoiceText = "out122222";
+
+                          // final loaded = Provider.of<Monuments>(
+                          //   context,
+                          //   listen: false,
+                          // ).findByName(value);
+                          // if (value == loaded.monumentName) {
+                          //   _newVoiceText = loaded.article;
+                          // }
+                          //  else {
+                          //   _newVoiceText = "empty";
+                          // }
+                        } else {
+                          _newVoiceText = "out";
+                        }
+                      }));
+            return  SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(children: [
+                    _inputSection(),
+                    _btnSection(),
+                  ]));
+            })));
   }
+
   Widget _inputSection() => Container(
       alignment: Alignment.topCenter,
-      padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),   
-      child:
-       Text(
-         _newVoiceText
-        // onChanged: (String value) {
-        //   _onChange(value);
-        // },
-      )
- 
-      );
+      padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
+      child: Text(_newVoiceText
+          // onChanged: (String value) {
+          //   _onChange(value);
+          // },
+          ));
 
   Widget _btnSection() {
-      return Container(
-          padding: EdgeInsets.only(top: 50.0),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            _buildButtonColumn(Colors.green, Colors.greenAccent,
-                Icons.play_arrow, 'PLAY', _speak),
-            _buildButtonColumn(
-                Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
-          ])
-          );
-
+    return Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          _buildButtonColumn(Colors.green, Colors.greenAccent, Icons.play_arrow,
+              'PLAY', _speak),
+          _buildButtonColumn(
+              Colors.red, Colors.redAccent, Icons.stop, 'STOP', _stop),
+        ]));
   }
+
   Column _buildButtonColumn(Color color, Color splashColor, IconData icon,
       String label, Function func) {
     return Column(
@@ -177,5 +192,4 @@ class _TextAudioState extends State<TextAudio> {
                       color: color)))
         ]);
   }
-
 }
